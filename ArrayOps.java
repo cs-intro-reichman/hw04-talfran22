964cf485b52756174316bdb4b142dbdccd88b324
Tal Frankenthal <154238085+talfran22@users.noreply.github.com>
2024-01-11 17:05:57 +0200

    finish

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,86 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+        int[] array = {2, 3, 1, 4, 0, 4, 4, 4, 4};
+        int[] array2 = {0,1,2,3,4};
+        System.out.println((findMissingInt(array)));
+        System.out.println(secondMaxValue(array));
+        System.out.println(containsTheSameElements(array, array2));
+        System.out.println(isSorted(array));
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        for(int i = 0 ; i < array.length ; i++)
+        {
+            if(isInArray(array, i) == false)
+                return i;
+
+        }
+        return array.length;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int[] nobig = new int[array.length - 1];
+        int max = array[0];
+        int j = 0;
+        for(int i = 1; i < array.length; i++){
+            if(array[i] < max){
+                nobig[j] = array[i];
+                j++;
+            }
+            if(array[i] > max){
+                nobig[j] = max;
+                max = array[i];
+                j++;
+            }
+        }
+        max = nobig[0];
+        for(int k = 1; k < nobig.length ; k++){
+            if(nobig[k] > max)
+                max = nobig[k];
+        }
+        return max;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        for(int i = 0; i < array1.length ; i++){
+            if(isInArray(array2, array1[i]) == false)
+                return false;
+        }
+        return true;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        if(array[0] > array[1]){
+            for(int i = 1; i < array.length - 1; i++){
+                if(array[i] < array[i + 1]){
+                    return false;
+                }
+            }
+        }
+        if(array[0] < array[1]){
+            for(int i = 1; i < array.length - 1; i++){
+                if(array[i] > array[i + 1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isInArray(int [] array, int num) {
+        for(int i = 0; i < array.length ; i++){
+            if(num == array[i]){
+                return true;
+            }
+        }
+        return true;
+    }
+
+    public static void println(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,80 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        System.out.println(capVowelsLowRest("Hello World"));
+        System.out.println(camelCase("   HElLo World"));
+        println(allIndexOf("MMMM", 'M'));
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String fix = "";
+        for(int i = 0; i < string.length() ; i ++){
+            if(string.charAt(i) == 'a' || string.charAt(i) == 'e' || string.charAt(i) == 'o' || string.charAt(i) == 'u' || string.charAt(i) == 'i')
+                fix += (char)(string.charAt(i) - 32) ;
+            else{
+                if(string.charAt(i) == ' ')
+                    fix += ' ';
+                else{
+                if(string.charAt(i) == 'A' || string.charAt(i) == 'E' || string.charAt(i) == 'O' || string.charAt(i) == 'U' || string.charAt(i) == 'I')
+                  fix += string.charAt(i);
+                else{
+                    if(string.charAt(i) > 64 && string.charAt(i) < 91)
+                       fix += (char)(string.charAt(i) + 32);
+                    else
+                       fix += string.charAt(i);
+                    }
+                }
+            }
+        }
+        return fix;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String fix = "";
+        for(int i = 0 ; i < string.length() ; i ++){
+            if(string.charAt(i) != ' '){
+                if(string.charAt(i) > 64 && string.charAt(i) < 91)
+                    fix += (char)(string.charAt(i) + 32);
+                else{
+                    fix += string.charAt(i);
+                }
+            }
+            else{
+                if(string.charAt(i+1) != ' ' && fix != ""){
+                    if((int)(string.charAt(i+1)) > 96 && (int)(string.charAt(i+1)) < 122)
+                        fix += (char)(string.charAt(i+1) - 32);
+                    else{
+                        fix += string.charAt(i+1);
+                    }
+                    
+                    i++;
+                }
+            }
+        }
+        return fix;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int sum = 0;
+        for(int i = 0 ; i < string.length() ; i++){
+            if(chr == string.charAt(i))
+                sum++;
+        }
+        int[] arr = new int[sum];
+        int k = 0;
+        for(int j = 0 ; j < string.length(); j++){
+            if(string.charAt(j) == chr){
+                arr[k] = j;
+                k++;
+            }
+
+        }
+        return arr;
+    }
+    public static void println(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
